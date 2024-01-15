8b111922b45f44e4e81b42b620cbc52ab6366737
Eye-Mans <laluiman468@gmail.com>
2024-01-15 14:01:03 +0700

    first commit

---
+++ src/JS/script.js
@@ -0,0 +1,21 @@
+// navbar-fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const navbarfixed = header.offsetTop;
+
+    if (window.pageYOffset > navbarfixed){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// button  hum
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector("#menu")
+hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('hidden');
+});
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+         x : "#3b82f6"
+      },
+    },
+  },
+  plugins: [],
+}
+
